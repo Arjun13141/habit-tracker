c00fae178e92cefc59f7a438f639ec60212087ae
Arjun13141 <rajputarjun.4002@gmail.com>
2026-01-21 05:26:29 +0000

    Fix static files configuration

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn config.wsgi --log-file -
+web: python manage.py collectstatic --noinput && gunicorn config.wsgi
 release: python manage.py migrate --noinput
--- config/settings.py
+++ config/settings.py
@@ -163,4 +163,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+# STATIC_URL = "static/"
